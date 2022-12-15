1b9dbac213db8a2579114b00cbc378263522c152
Vishwa-vijay-shahi <115209572+Vishwa-vijay-shahi@users.noreply.github.com>
2022-12-15 11:07:47 +0530

    updates

--- script.js
+++ script.js
@@ -1,107 +1,29 @@
-var input = document.getElementById('input');
-var result = document.getElementById('result');
-var inputType = document.getElementById('inputType');
-var resultType = document.getElementById('resultType');
-var inputTypeValue,resultTypeValue;
-input.addEventListener("keyup",myResult);
-inputType.addEventListener("change",myResult);
-resultType.addEventListener("change",myResult);
-//taking initial value
- inputTypeValue=inputType.value;
-resultTypeValue=resultType.value;
+const from = document.getElementById("from")
+const to = document.getElementById("to")
+const inputNumber = document.getElementById("input-number")
+const resultBox = document.getElementById("result-box")
 
-function myResult(){
-    // when we change the input and output type value we need to update 
-    //input typevalue and resulttypevalue 
-    inputTypeValue=inputType.value;
-    resultTypeValue=resultType.value;
-    //now compare the input and resulttype value and add fromula 
-    if(inputTypeValue==="Decimal"&& resultTypeValue==="Hexadecimal"){
-        var x = parseInt(input.value,10);
-    result.value= Number(x).toString(16);
-     
-      
-    }
-    else if(inputTypeValue==="Decimal"&& resultTypeValue==="Octal"){
-        var x = parseInt(input.value,10);
-    result.value= Number(x).toString(8);
-     
+document.getElementById("convert").addEventListener("click", () => {
+    //check from
+    let fromValue = from.value 
+    // check to
+    let toValue = to.value
+    // check input number
+   let inputValue = inputNumber.value
+   // convert input number "from" to "to"
+   let result = convertBase(fromValue, toValue, inputValue)
+   //show the result in result box
+   resultBox.innerText = result
+})
 
-    }
-    else if(inputTypeValue==="Decimal"&& resultTypeValue==="Binary"){
-        var x = parseInt(input.value,10);
-    result.value= Number(x).toString(2);
-     
-
-    }
-    else if(inputTypeValue==="Decimal"&& resultTypeValue==="Decimal"){
-        result.value=input.value;
-
-}
-if(inputTypeValue==="Hexadecimal"&& resultTypeValue==="Decimal"){
-    var x = parseInt(input.value,16);
-    result.value= Number(x).toString(10);
-     
-}
-else if(inputTypeValue==="Hexadecimal"&& resultTypeValue==="Octal"){
-    var x = parseInt(input.value,16);
-    result.value= Number(x).toString(8);
-     
-
+function convertBase(fromBase, toBase, num) {
+    let d = parseInt(num, fromBase) // converting number into base10
+    let res = d.toString(toBase) //converting base10 to required base
+    return res
 }
 
-else if(inputTypeValue==="Hexadecimal"&& resultTypeValue==="Hexadecimal"){
-    result.value=input.value;
-}
-else if(inputTypeValue==="Hexadecimal"&& resultTypeValue==="Binary"){
-    var x = parseInt(input.value,16);
-    result.value= Number(x).toString(2);
-     
-
-}
-
-
-if(inputTypeValue==="Octal"&& resultTypeValue==="Decimal"){
-    var x = parseInt(input.value,8);
-    result.value= Number(x).toString(10);
-     
-}
-else if(inputTypeValue==="Octal"&& resultTypeValue==="Hexadecimal"){
-    var x = parseInt(input.value,8);
-    result.value= Number(x).toString(16);
-     
-
-}
-else if(inputTypeValue==="Octal"&& resultTypeValue==="Octal"){
-    result.value=input.value;
-
-
-}
-else if(inputTypeValue==="Octal"&& resultTypeValue==="Binary"){
-    var x = parseInt(input.value,8);
-    result.value= Number(x).toString(2);
-     
-
-
-}
-if(inputTypeValue==="Binary"&& resultTypeValue==="Hexadecimal"){
-    var x = parseInt(input.value,2);
-    result.value= Number(x).toString(16);
-}
-else if(inputTypeValue==="Binary"&& resultTypeValue==="Decimal"){
-    var x = parseInt(input.value,2);
-    result.value= Number(x).toString(10);
-     
-
-}
-else if(inputTypeValue==="Binary"&& resultTypeValue==="Octal"){
-    var x = parseInt(input.value,2);
-    result.value= Number(x).toString(8);
-
-}
-else if(inputTypeValue==="Binary"&& resultTypeValue==="Binary"){
-    result.value=input.value;
-
-
-}
-}
+document.getElementById("swap").addEventListener("click", () => {
+    let temp = from.value
+    from.value = to.value
+    to.value = temp
+})
